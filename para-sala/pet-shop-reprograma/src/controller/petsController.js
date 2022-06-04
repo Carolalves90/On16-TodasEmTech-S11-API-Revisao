d888224840ef122981567f39ef57d68b7b2c0767
Carol Alves <prof.carolalves90@gmail.com>
2022-06-04 16:53:25 -0300

    :tada: build: código patch

--- para-sala/pet-shop-reprograma/src/controller/petsController.js
+++ para-sala/pet-shop-reprograma/src/controller/petsController.js
@@ -16,6 +16,33 @@ const postPet = (req, res) => {
   })
 }
 
+const updateName = (req, res) => {
+    
+    let idPetRequest = req.params.id
+    let nomeFantasiaRequest = req.body.nomeFantasia
+    
+    const petFound = petsJson.find(pet => pet.id == idPetRequest) // encontrando o pet
+    const petIndex = petsJson.indexOf(petFound) // identificando o indice do pet no meu array
+    
+    if (petIndex >= 0){
+    petFound.nomeFantasia = nomeFantasiaRequest
+                //encontrou //remove //adiciona
+    petsJson.splice(petIndex, 1, petFound)
+    
+    fs.writeFile("./src/models/pets.json", JSON.stringify(petsJson), 'utf8', function (err) { // gravando novo pet no array de pets
+        if (err) {
+            res.status(500).send({ message: err })
+        } else {
+            console.log("Teu arquivo foi alterado");
+            const petUpdated = petsJson.find(pet => pet.id == idPetRequest)
+            res.status(200).send(petUpdated)
+        }})
+    } else {
+        res.status(404).send({ message: "Nao encontramos esse petshop, cadastra ai miga"})
+        }
+    }
+
 module.exports = {
-  postPet
+  postPet,
+  updateName
 };
